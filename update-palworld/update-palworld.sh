f47c96f1c86da7e2e149f02b800a56397e48df21
Nayuengin <65211260+Nayuengin@users.noreply.github.com>
2024-01-25 22:14:40 +0900

    Update script to use variable for AppID

--- update-palworld/update-palworld.sh
+++ update-palworld/update-palworld.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-# このスクリプトは、SteamCMDを使って、Palworldのゲームサーバーを更新します。
+# このスクリプトは、SteamCMDを使って、対象ゲーム用のサーバーを更新するものです。
+APP_ID=2394010 # PalworldのAppID
 
 echo "現在のバージョンを確認します。"
 date
-OLD_BUILD_ID=`steamcmd +login anonymous +app_status 2394010 +quit | grep -e "BuildID" | awk '{print $8}'`
+OLD_BUILD_ID=`steamcmd +login anonymous +app_status $APP_ID +quit | grep -e "BuildID" | awk '{print $8}'`
 echo "現在のビルドID: $OLD_BUILD_ID"
 
 echo "サーバーの更新を開始します。"
-steamcmd +login anonymous +app_update 2394010 validate +quit > /dev/null
+steamcmd +login anonymous +app_update $APP_ID validate +quit
 
 echo "更新後のバージョンを確認します。"
-NEW_BUILD_ID=`steamcmd +login anonymous +app_status 2394010 +quit | grep -e "BuildID" | awk '{print $8}'`
+NEW_BUILD_ID=`steamcmd +login anonymous +app_status $APP_ID +quit | grep -e "BuildID" | awk '{print $8}'`
 echo "更新後のBuildID: $NEW_BUILD_ID"
 
 # バージョンが変わったかどうかを確認する
