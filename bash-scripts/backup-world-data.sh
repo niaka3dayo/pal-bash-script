34e5f69acb461e9c2b88481dc510f31c0b35021c
Nayuengin <65211260+Nayuengin@users.noreply.github.com>
2024-01-26 21:34:14 +0900

    Add backup-world-data.sh script and update watch-memory.sh script

---
+++ bash-scripts/backup-world-data.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# セーブデータのパス
+WORLD_SAVE_PATH="/home/ubuntu/Steam/steamapps/common/PalServer/Pal/Saved/SaveGames"
+
+# セーブデータのバックアップ保存先のパス
+WORLD_BACKUP_PATH="/home/ubuntu/PalSeverBackup"
+
+# 作成するバックアップの最大数
+MAX_BACKUP_COUNT=5
+
+# ワールドデータをバックアップする関数
+# zipコマンドを使用しているため、zipコマンドが使用可能である必要がある
+backup_world_data() {
+    # バックアップ保存先のディレクトリが存在しない場合は作成
+    if [ ! -d $WORLD_BACKUP_PATH ]; then
+        mkdir -p $WORLD_BACKUP_PATH
+    fi
+
+    # バックアップ保存先のディレクトリにzip形式でバックアップを作成
+    # ファイル名形式は PalWorldSave_YYYYMMDD-HHMM-S.zip
+    zip -r $WORLD_BACKUP_PATH/PalWorldSave_$(TZ=Asia/Tokyo date '+%Y%m%d-%H%M-%S').zip $WORLD_SAVE_PATH
+    echo "World data backup created: $WORLD_BACKUP_PATH/PalWorldSave_$(TZ=Asia/Tokyo date '+%Y%m%d-%H%M-%S').zip"
+
+    # バックアップがMAX_BACKUP_COUNT以上存在する場合は古いものから削除
+    BACKUP_COUNT=$(ls $WORLD_BACKUP_PATH | wc -l)
+    if [ $BACKUP_COUNT -gt $MAX_BACKUP_COUNT ]; then
+        rm $(ls -t $WORLD_BACKUP_PATH | tail -n $(($BACKUP_COUNT - $MAX_BACKUP_COUNT)))
+    fi
+}
+
+# バックアップの作成を実行
+backup_world_data
--- bash-scripts/watch-memory.sh
+++ bash-scripts/watch-memory.sh
@@ -12,15 +12,6 @@ RCON_PORT="25575"
 # RCONの管理者用パスワード 適時変更
 RCON_PASSWORD="password"
 
-# セーブデータのパス 適時変更
-WORLD_SAVE_PATH="/home/ubuntu/Steam/steamapps/common/PalServer/Pal/Saved/SaveGames"
-
-# セーブデータのバックアップ保存先のパス 適時変更
-WORLD_BACKUP_PATH="/home/ubuntu/PalSeverBackup"
-
-# 作成するバックアップの最大数
-MAX_BACKUP_COUNT=5
-
 # ワールド内にメッセージを送信する関数
 # mcronコマンドを使用しているため、RCONが有効で、mcrconコマンドが使用可能である必要がある
 # 他のRCONクライアントを使用する場合は、この関数を書き換える必要がある
@@ -37,26 +28,6 @@ send_world_message() {
 save_world_data() {
     mcrcon -H $RCON_IP -P $RCON_PORT -p $RCON_PASSWORD "Save"
     echo "Save command sent to world."
-}
-
-# ワールドデータをバックアップする関数
-# zipコマンドを使用しているため、zipコマンドが使用可能である必要がある
-backup_world_data() {
-    # バックアップ保存先のディレクトリが存在しない場合は作成
-    if [ ! -d $WORLD_BACKUP_PATH ]; then
-        mkdir -p $WORLD_BACKUP_PATH
-    fi
-
-    # バックアップ保存先のディレクトリにzip形式でバックアップを作成
-    # ファイル名形式は PalWorldSave_YYYYMMDD-HHMM-S.zip
-    zip -r $WORLD_BACKUP_PATH/PalWorldSave_$(TZ=Asia/Tokyo date '+%Y%m%d-%H%M-%S').zip $WORLD_SAVE_PATH
-    echo "World data backup created: $WORLD_BACKUP_PATH/PalWorldSave_$(TZ=Asia/Tokyo date '+%Y%m%d-%H%M-%S').zip"
-
-    # バックアップがMAX_BACKUP_COUNT以上存在する場合は古いものから削除
-    BACKUP_COUNT=$(ls $WORLD_BACKUP_PATH | wc -l)
-    if [ $BACKUP_COUNT -gt $MAX_BACKUP_COUNT ]; then
-        rm $(ls -t $WORLD_BACKUP_PATH | tail -n $(($BACKUP_COUNT - $MAX_BACKUP_COUNT)))
-    fi
 }
 
 # 現在のメモリ使用量を取得
@@ -87,7 +58,7 @@ if [ $MEMORY_USAGE -gt $THRESHOLD ]; then
   sudo systemctl stop palserver.service
   bash ./send-discord-message.sh "ワールドサーバーを停止しました。"
   # サーバー停止後にワールドデータのバックアップを作成して10秒待機
-  backup_world_data
+  bash ./backup-world-data.sh
   sleep 10
   # ワールドサーバーを起動して10秒待機
   sudo systemctl start palserver.service
